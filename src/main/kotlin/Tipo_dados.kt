b4dcb81910122bb01a90132a9841bce7a21b4b4c
Lais Pinto <laisp19@hotmail.com>
2022-09-07 17:59:28 -0300

    - Dia 2 dos estudos de kotlin

--- src/main/kotlin/Tipo_dados.kt
+++ src/main/kotlin/Tipo_dados.kt
@@ -11,7 +11,17 @@ fun main() {
     var short: Short = 10
     var byte: Byte = 10
 
-    var idade = 40
+    // Possível declarar uma variável sem tipo, desde que seja incializada.
+    // Tipo de dado é definido através da inferência.
+    var idade = 18
+
+    // Isso não pode ser feito, a variável não sabe nem o valor, nem o tipo
+    // var idade2
+
+    // Variável declarada, mas não inicializada
+    var idade3: Int
+
+
 
     println("Double MAX ${Double.MAX_VALUE} - MIN ${Double.MIN_VALUE}")
     println("Float MAX ${Float.MAX_VALUE} - MIN ${Float.MIN_VALUE}")
@@ -20,3 +30,14 @@ fun main() {
     println("Short MAX ${Short.MAX_VALUE} - MIN ${Short.MIN_VALUE}")
     println("Byte MAX ${Byte.MAX_VALUE} - MIN ${Byte.MIN_VALUE}")
 }
+/**
+ * -- Anotações --
+ * O tamanho Boolean pode ser representado somente com 1 bit, true ou false
+ * Porém o tamanho é depende da JVM e a especificaçacão não deixa claro
+ *
+ * String
+ * O tamanho de uma String depende de seu conteúdo.
+ *
+ * Char
+ * O tamanho de um char, assim como Boolean, também é variável e dependente da JVM
+ */
